566a74773409bea931a4a292673ee87cf130a773
Sniper <sniper_zheng@163.com>
2025-03-30 22:38:39 +0800

    Revert "数据库连接超时问题修正"
    This reverts commit 7947183d03e7fa166ef679437decbb2b8240a8b3.

--- beauty-backend/src/config/mongodb.js
+++ beauty-backend/src/config/mongodb.js
@@ -10,20 +10,10 @@ function getMongoUri() {
   if (process.env.NODE_ENV === 'production') {
     return process.env.MONGODB_URI;
   }
-
-  // 如果设置了用户名和密码，添加到连接字符串中
-  if (MONGO_INITDB_ROOT_USERNAME && MONGO_INITDB_ROOT_PASSWORD) {
-    return `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}?authSource=admin`;
-  }
   
   return `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`;
 }
 
 module.exports = {
   uri: getMongoUri(),
-  options: {
-    serverSelectionTimeoutMS: 5000, // 超时时间设置为 5 秒
-    socketTimeoutMS: 45000, // Socket 超时时间
-    connectTimeoutMS: 10000, // 连接超时时间
-  }
 }; 
